42fa50f7b964d1537f153acd4303249383ceb21e
Luukas <luukasniemin@gmail.com>
2023-12-18 13:51:24 +0200

    Added feature to /man command to search for subcommands

--- src/bot/commands/utility/man.ts
+++ src/bot/commands/utility/man.ts
@@ -2,19 +2,53 @@ import { SlashCommandBuilder, Client, ChatInputCommandInteraction, EmbedBuilder,
 
 export = {
     data: new SlashCommandBuilder()
-    .setName("man")
-    .setDescription("Manual page for a command.")
-    .addStringOption(option => 
-        option
-        .setName("command")
-        .setDescription("Command manual page to search for")
-        .setRequired(true)    
-    ),
+        .setName("man")
+        .setDescription("Manual page for a command.")
+        .addStringOption(option =>
+            option
+                .setName("command")
+                .setDescription("Command manual page to search for")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("subcommand")
+                .setDescription("Search for a subcommand manual page")
+        ),
 
     async execute(interaction: ChatInputCommandInteraction, client: Client) {
+        if (interaction.options.getString("subcommand")) {
+            const targetCommand: string = interaction.options.getString("command")!
+            const subCommand = interaction.options.getString("subcommand")
+
+            if (!client.commands.get(targetCommand)) return await interaction.reply({
+                content: `Hmm, command **${targetCommand}** does not exist\n`
+            })
+
+            const commandOptions = client.commands.get(targetCommand).data.options
+            const foundSubcommand = commandOptions.filter((option: SlashCommandSubcommandBuilder | any) => {
+                if (option instanceof SlashCommandSubcommandBuilder && option.name === subCommand) {
+                    return option
+                }
+            })[0]
+
+            console.log(foundSubcommand)
+
+            if (!foundSubcommand) {
+                return await interaction.reply({ content: `Hmm, subcommand **${subCommand}** does not exist on command **${targetCommand}**\n` })
+            }
+
+            const manualEmbed = new EmbedBuilder()
+                .setDescription(`\`\`NAME\`\`\n> ${targetCommand} ${foundSubcommand.name}\n\n\`\`DESCRIPTION\`\`\n> ${foundSubcommand.description}\n`)
+                .setFooter({ text: `Requested by ${interaction.user.displayName}`, iconURL: interaction.user.displayAvatarURL() })
+                .setTimestamp()
+
+            return await interaction.reply({ embeds: [manualEmbed] })
+        }
+
         const targetCommand: string = interaction.options.getString("command")!
 
-        if(!client.commands.get(targetCommand)) return await interaction.reply({
+        if (!client.commands.get(targetCommand)) return await interaction.reply({
             content: `Hmm, command **${targetCommand}** does not exist\n`
         })
 
@@ -22,18 +56,15 @@ export = {
         let commandSubCommands: string[] = []
 
         command.data.options.forEach((option: any) => {
-            console.log(option instanceof SlashCommandSubcommandBuilder)
-            if(option instanceof SlashCommandSubcommandBuilder) {
+            if (option instanceof SlashCommandSubcommandBuilder) {
                 commandSubCommands.push(option.name)
             }
         })
 
-        console.log(commandSubCommands.join(", "))
-
         const manualEmbed = new EmbedBuilder()
-        .setDescription(`\`\`NAME\`\`\n> ${command.data.name}\n\n\`\`DESCRIPTION\`\`\n> ${command.data.description}\n\n${commandSubCommands.length > 0 ? `\`\`SUBCOMMANDS\`\`\n> ${commandSubCommands.join(",")}\n` : ""}`)
-        .setFooter({ text: `Requested by ${interaction.user.displayName}`, iconURL: interaction.user.displayAvatarURL() })
-        .setTimestamp()
+            .setDescription(`\`\`NAME\`\`\n> ${command.data.name}\n\n\`\`DESCRIPTION\`\`\n> ${command.data.description}\n\n${commandSubCommands.length > 0 ? `\`\`SUBCOMMANDS\`\`\n> ${commandSubCommands.join(",")}\n` : ""}`)
+            .setFooter({ text: `Requested by ${interaction.user.displayName}`, iconURL: interaction.user.displayAvatarURL() })
+            .setTimestamp()
 
         console.log(client.commands.get(targetCommand).data.options)
         await interaction.reply({ embeds: [manualEmbed] })
